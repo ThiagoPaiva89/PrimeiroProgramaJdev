340b7b5d2c090ed79f80a3ff66368481f478cca4
ThiagoPaiva89 <thiagoinaciopaiva@gmail.com>
2019-08-07 00:31:30 -0300

    Adequação do projeto à orientação a objetos

--- PrimeiroProgramaJdev/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ PrimeiroProgramaJdev/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -1,38 +1,108 @@
 package cursoJava.executavel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
+
+import cursoJava.classes.Aluno;
+import cursoJava.classes.Disciplina;
+import cursoJava.constantes.StatusAluno;
 
 public class PrimeiraClasseJava {
 	/*Main é um método auto executável em java*/
 	public static void main(String[] args) {
-		/*Cálculo de média*/
 		
-		String nota1 = JOptionPane.showInputDialog("Informe a nota do primeiro bimestre, com duas casas decimais");
-		String nota2 = JOptionPane.showInputDialog("Informe a nota do segundo bimestre, com duas casas decimais");
-		String nota3 = JOptionPane.showInputDialog("Informe a nota do terceiro bimestre, com duas casas decimais");
-		String nota4 = JOptionPane.showInputDialog("Informe a nota do quarto bimestre, com duas casas decimais");
+		List<Aluno> alunos = new ArrayList<Aluno>();
 		
-		/*Converte strings para números reais*/
-		double dNota1 = Double.parseDouble(nota1);
-		double dNota2 = Double.parseDouble(nota2);
-		double dNota3 = Double.parseDouble(nota3);
-		double dNota4 = Double.parseDouble(nota4);
+		List<Aluno> alunosAprovados = new ArrayList<Aluno>();
+		List<Aluno> alunosRecuperacao = new ArrayList<Aluno>();
+		List<Aluno> alunosReprovados = new ArrayList<Aluno>();
 		
-		/*Calcula média do aluno*/
-		double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
 		
-		if	(media >= 50) {
+		for (int qtd = 1; qtd <= 5; qtd++) {
+	
+		String nome = JOptionPane.showInputDialog("Digite o nome do " +qtd+ "o Aluno");
+		String dataNascimento = JOptionPane.showInputDialog("Aluno, digite sua data de nascimento: ");
+		String registroGeral = JOptionPane.showInputDialog("Aluno, digite seu RG: ");
+		String cpf = JOptionPane.showInputDialog("Aluno, digite seu CPF: ");
+		String mae = JOptionPane.showInputDialog("Nome da mãe");
+		String pai = JOptionPane.showInputDialog("Nome do pai");
+		String dataMatricula = JOptionPane.showInputDialog("Data de matrícula");
+		String nomeEscola = JOptionPane.showInputDialog("Nome da escola:");
+		String serie = JOptionPane.showInputDialog("Serie matriculado");
+	
+		
+		
+		Aluno aluno1 = new Aluno(); /*Aluno joão*/
+		aluno1.setNome(nome);
+		aluno1.setDataNascimento(dataNascimento);
+		aluno1.setRegistroGeral(registroGeral);
+		aluno1.setNumeroCpf(cpf);
+		aluno1.setNomeMae(mae);
+		aluno1.setNomePai(pai);
+		aluno1.setDataMatricula(dataMatricula);
+		aluno1.setNomeEscola(nomeEscola);
+		aluno1.setSerieMatriculado(serie);
+		
+		for (int posicao = 1; posicao <= 4; posicao++) {
+			String nomeDisciplina = JOptionPane.showInputDialog("Digite o nome da disciplina " + posicao + ": ");
+			String notaDisciplina = JOptionPane.showInputDialog("Digite a nota da disciplina " + posicao + ": ");
 			
-			if (media >= 70) {
-				JOptionPane.showMessageDialog(null, "Parabéns, você foi aprovado com a media de: " + media);
-			} else {
-				JOptionPane.showMessageDialog(null, "Infelizamente você obteve a média de " + media + " que é abaixo da média para ser aprovado. Favor, marcar prova de recuperação");
-			}	
-		}else {
+			Disciplina disciplina = new Disciplina();
+			disciplina.setDisciplina(nomeDisciplina);
+			disciplina.setNota(Double.valueOf(notaDisciplina));
+			
+			aluno1.getDisciplinas().add(disciplina);
+		}
 		
-			JOptionPane.showMessageDialog(null, "Sua média foi: " + media + "Com esta nota você reprovou direto");
+		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina");
+		
+		if (escolha == 0) { /*Opção sim = 0*/
+			
+			int continuarRemover = 0;
+			int posicao = 1;
+			
+			while(continuarRemover == 0) {
+			
+			String disciplinaRemover = JOptionPane.showInputDialog("Qual deseja remover, 1, 2, 3 ou 4?");
+			aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
+			posicao ++;
+			continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover?");
+			}
+		}
+		
+		alunos.add(aluno1);
 		
 		}
+		
+		for (Aluno aluno : alunos) {
+			
+			if (aluno.getResultadoAluno().equalsIgnoreCase(StatusAluno.APROVADO)) {
+				alunosAprovados.add(aluno);
+			}else if (aluno.getResultadoAluno().equalsIgnoreCase(StatusAluno.RECUPERACAO)) {
+				alunosRecuperacao.add(aluno);
+			}else {
+				alunosReprovados.add(aluno);
+			}
+			
+		}
+		
+		System.out.println("----------------------Lista dos Aprovados------------------");
+			for	(Aluno aluno : alunosAprovados) {
+				System.out.println("Resultado = " + aluno.getResultadoAluno() + " com média de " + aluno.getMediaNota());
+				
+			}
+			
+		System.out.println("----------------------Lista dos Reprovados------------------");
+			for	(Aluno aluno : alunosReprovados) {
+				System.out.println("Resultado = " + aluno.getResultadoAluno() + " com média de " + aluno.getMediaNota());
+				
+			}
+		System.out.println("----------------------Lista dos em Recuperação------------------");
+			for	(Aluno aluno : alunosRecuperacao) {
+				System.out.println("Resultado = " + aluno.getResultadoAluno() + " com média de " + aluno.getMediaNota());
+				
+			}	
 	}
-	
 }
